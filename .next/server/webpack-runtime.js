fd7473b7edfb3f53964eac823569e4600166a749
staaskemmetxcg <staaskemmetxcg@hotmail.com>
2026-05-02 10:03:11 -0300

    Updated netlify.toml

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("dba39614dbddf11f")
+/******/ 		__webpack_require__.h = () => ("df475ed8cabefe4a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
